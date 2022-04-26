517b9386bccebb6cd47d44ac4fd2a482b0555fc7
Vax Chen <canadavaxchen@gmail.com>
2022-04-26 12:46:46 -0400

    minor change
    Signed-off-by: Vax Chen <canadavaxchen@gmail.com>

--- src/main/java/com/vaxchen/app/diff/Controller/ApiController.java
+++ src/main/java/com/vaxchen/app/diff/Controller/ApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +22,14 @@ public class ApiController {
 	@Autowired
 	public DataRepo dataRepo;
 	
-	@PostMapping( value = "/v1/diff/{id}/left", produces = "application/json" )
+	@PutMapping( value = "/v1/diff/{id}/left", produces = "application/json" )
 	public String putLeft(@RequestBody Data data) {
 		data.setId(1);
 		dataRepo.save(data);
 		return "left is saved";
 	}
 	
-	@PostMapping( value = "/v1/diff/{id}/right", produces = "application/json" )
+	@PutMapping( value = "/v1/diff/{id}/right", produces = "application/json" )
 	public String putRight(@RequestBody Data data) {
 		data.setId(2);
 		dataRepo.save(data);
